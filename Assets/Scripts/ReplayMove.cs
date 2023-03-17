b7619a2f0cf00250308334366fbf23ed950dcd41
KrcilJ <krcil.jakub@gmail.com>
2023-03-17 15:29:33 +0000

    Game reaplay, position based on a clicked move

--- Assets/Scripts/ReplayMove.cs
+++ Assets/Scripts/ReplayMove.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+
 public class ReplayMove : MonoBehaviour
 {
     [SerializeField] private GameObject replayMove;
     [SerializeField] private GameObject parent;
     [SerializeField] private GameObject replayMoveIndex;
+    private Grid grid;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,26 +19,37 @@ public class ReplayMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void generateReplayMoveIndex( string index) {
+    void Awake()
+    {
+        grid = GameObject.FindGameObjectWithTag("GameController").GetComponent<Grid>();
+    }
+    public void generateReplayMoveIndex(string index)
+    {
         var spawnedMove = Instantiate(replayMoveIndex, new Vector3(0, 0), Quaternion.identity);
         spawnedMove.transform.SetParent(parent.transform);
         TextMeshProUGUI mText = spawnedMove.GetComponent<TextMeshProUGUI>();
         mText.text = index;
     }
-    public void generateReplayMove(string moveText){
-       
+    public void generateReplayMove(string moveText, int index)
+    {
+
         var spawnedMove = Instantiate(replayMove, new Vector3(0, 0, -1), Quaternion.identity);
         spawnedMove.transform.SetParent(parent.transform);
         TextMeshProUGUI mText = spawnedMove.GetComponent<TextMeshProUGUI>();
         mText.text = moveText;
+        spawnedMove.name = $"Move {index}";
     }
     //  void OnMouseDown()
     // {
     //     Debug.Log("Pressed on " + GetComponent<TextMeshProUGUI>().text);
     // }
-    public void clicked () {
-        Debug.Log("Pressed on " + GetComponent<TextMeshProUGUI>().text);
+    public void clicked()
+    {
+        int index = Convert.ToInt32(this.name.Substring(4));
+        Debug.Log(index);
+        grid.replayNumMoves(index + 1);
+
     }
 }
